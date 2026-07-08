c318b8cfcb9d43c5f9a57ca89f6d0fecf28792e5
reddymahendra166-ui <reddymahendra166@gmail.com>
2026-07-08 11:13:23 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,16 @@
+# Data Types in Python
+
+name = "Mahendra"      # String
+age = 21               # Integer
+height = 5.8           # Float
+is_student = True      # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
